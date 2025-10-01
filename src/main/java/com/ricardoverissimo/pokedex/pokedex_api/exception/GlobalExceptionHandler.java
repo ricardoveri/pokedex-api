03bd4d0dd04d83ecdab6c7282e38dba90da1a68a
Ricardo Veríssimo Pereira <ricardoverissimo1000@gmail.com>
2025-10-01 01:13:05 -0300

    retirada do java.util.objects

--- src/main/java/com/ricardoverissimo/pokedex/pokedex_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ricardoverissimo/pokedex/pokedex_api/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
