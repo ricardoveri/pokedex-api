3248a44250be3f27560cf80cca06a9867eb64b5e
Ricardo Veríssimo Pereira <ricardoverissimo1000@gmail.com>
2025-10-01 01:22:05 -0300

    Adicionando UserDetailsService e AuthenticationProvider.

--- src/main/java/com/ricardoverissimo/pokedex/pokedex_api/config/SecurityBeansConfig.java
+++ src/main/java/com/ricardoverissimo/pokedex/pokedex_api/config/SecurityBeansConfig.java
@@ -1,6 +1,9 @@
 package com.ricardoverissimo.pokedex.pokedex_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,9 +12,23 @@ import org.springframework.security.config.annotation.authentication.configurati
 @Configuration
 public class SecurityBeansConfig {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityBeansConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
