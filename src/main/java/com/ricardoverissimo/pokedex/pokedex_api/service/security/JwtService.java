7b1d1fe4ff944a5be6ba6b053dc61b39d411cd16
Ricardo Veríssimo Pereira <ricardoverissimo1000@gmail.com>
2025-10-01 01:13:51 -0300

    ajustando espaços

--- src/main/java/com/ricardoverissimo/pokedex/pokedex_api/service/security/JwtService.java
+++ src/main/java/com/ricardoverissimo/pokedex/pokedex_api/service/security/JwtService.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
@@ -70,4 +69,5 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
 }
